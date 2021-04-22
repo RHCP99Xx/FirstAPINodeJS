0a0259658cb96f439382102312c3997f97df40d2
Edgar Hernández <47510635+RHCP99Xx@users.noreply.github.com>
2021-04-22 17:35:39 -0500

    Second commit
    I have an error while im trying to fetch with json placeholder. It shows that json format is incorrect

--- ejemplo-api-rest/src/index.js
+++ ejemplo-api-rest/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // Routes
 app.use(require('./routes/index'));
 app.use('/api/movies',require('./routes/movies'));
-
+app.use('/api/users', require('./routes/users'));
 // Starting the server
 app.listen(app.get('port'), () =>{
     console.log('Server on port ${3000}');
